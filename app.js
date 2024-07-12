a7b2bce4cbc7a085cd9cc214a3d315d9d8afc0ef
neeraj-tangariya <neeraj.tangariya@logicsimplified.com>
2024-07-12 14:22:27 +0530

    post request added

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 import express from "express";
 import createHomepageTemplate from "./views/index.js";
+import createListTemplate from "./views/list.js";
+import BOOKS_DATA from "./data/data.js";
+import createBookTemplate from "./views/book.js";
 
 // create app
 const app = express();
@@ -11,6 +14,27 @@ app.use(express.static("public"));
 // routes
 app.get("/", (req, res) => {
   res.send(createHomepageTemplate());
+});
+
+app.get("/books", (req, res) => {
+  res.send(createListTemplate());
+});
+
+app.post("/books", (req, res) => {
+  const { title, author } = req.body;
+  console.log(req.body);
+  const id = Math.random().toString();
+
+  BOOKS_DATA.push({ id, title, author });
+
+  res.redirect("/books/" + id);
+});
+
+app.get("/books/:id", (req, res) => {
+  const { id } = req.params;
+  const book = BOOKS_DATA.find((b) => b.id === id);
+
+  res.send(createBookTemplate(book));
 });
 
 // listen to port
--- views/index.js
+++ views/index.js
@@ -14,12 +14,26 @@ const createHomepageTemplate = () => /*html*/ `
 
       <main>
         <div class="book-list">
-          <!-- book list here later -->
+          <button hx-get="/books" hx-target=".book-list">Show Books</button>
         </div>
 
         <div class="add-book-form">
           <h2>What do you want to read?</h2>
-          <!-- form template here later -->
+          <form>
+            <input 
+              id="title" 
+              name="title"
+              placeholder="title" 
+              type="text" 
+            />
+            <input 
+              id="author" 
+              name="author"
+              placeholder="author" 
+              type="text" 
+            />
+            <button hx-post="/books" hx-target=".book-list ul" hx-swap="beforeend">Add</button>
+          </form>
         </div>
       </main>
     </body>
