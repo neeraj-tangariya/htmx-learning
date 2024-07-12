a88afdfcdf9a52c4262f4c5e242528fd486f4fb2
neeraj-tangariya <neeraj.tangariya@logicsimplified.com>
2024-07-12 12:12:07 +0530

    view setup

---
+++ views/index.js
@@ -0,0 +1,29 @@
+const createHomepageTemplate = () => /*html*/ `
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <title>My Reading List</title>
+      <script src="https://unpkg.com/htmx.org@2.0.0" 
+              integrity="sha384-wS5l5IKJBvK6sPTKa2WZ1js3d947pvWXbPJ1OmWfEuxLgeHcEbjUUA5i9V5ZkpCw" crossorigin="anonymous"></script>
+      <link rel="stylesheet" href="/styles.css">
+    </head>
+    <body>
+      <header>
+        <h1>My Reading List</h1>
+      </header>
+
+      <main>
+        <div class="book-list">
+          <!-- book list here later -->
+        </div>
+
+        <div class="add-book-form">
+          <h2>What do you want to read?</h2>
+          <!-- form template here later -->
+        </div>
+      </main>
+    </body>
+  </html>
+`;
+
+export default createHomepageTemplate;
